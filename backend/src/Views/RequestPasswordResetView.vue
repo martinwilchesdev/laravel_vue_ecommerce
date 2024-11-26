3ee9df42a48cc3ace6957e989504942bf0f2a6a2
Martin Wilches <martindotdev@gmail.com>
2024-11-25 21:19:43 -0500

    Vistas para el inicio de sesion

---
+++ backend/src/Views/RequestPasswordResetView.vue
@@ -0,0 +1,58 @@
+<script setup></script>
+
+<template>
+    <div
+        class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+    >
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2
+                class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900"
+            >
+                Reset password
+            </h2>
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form class="space-y-6" action="#" method="POST">
+                <div>
+                    <label
+                        for="email"
+                        class="block text-sm/6 font-medium text-gray-900"
+                        >Email address</label
+                    >
+                    <div class="mt-2">
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            autocomplete="email"
+                            required=""
+                            placeholder="Enter email"
+                            class="block w-full rounded-xs border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <div class="flex items-center justify-start">
+                        <div class="text-sm">
+                            <RouterLink
+                                :to="{ name: 'login' }"
+                                class="font-semibold text-indigo-600 hover:text-indigo-500"
+                            >Back to login page</RouterLink>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <button
+                        type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
--- backend/src/main.js
+++ backend/src/main.js
@@ -1,3 +1,4 @@
+import router from './router'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './index.css'
@@ -6,5 +7,5 @@ import App from './App.vue'
 const pinia = createPinia()
 const app = createApp(App)
 
-app.use(pinia)
+app.use(pinia).use(router)
 app.mount('#app')
